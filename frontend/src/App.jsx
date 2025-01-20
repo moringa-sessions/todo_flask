0518371ae0d86186751de8b2418c74e1788be6e6
k-koech <koechkelvin97@gmail.com>
2025-01-20 11:49:57 +0300

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import NoPage from "./pages/NoPage"
+import Profile from "./pages/Profile"
+import SingleTodo from "./pages/NoPage"
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import { Outlet } from 'react-router-dom'
+
+
+export default function Layout() {
+  return (
+    <div>
+      <Navbar/>
+      
+      <div className="min-h-[90vh] bg-gray-200 container mx-auto p-8">
+         <Outlet />
+      </div>
+
+      <Footer />
+      
+    </div>
+  )
+}
